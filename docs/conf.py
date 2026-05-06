67413293a1fb5cc8e608753b01c2e3904fc8426c
Farjana Yesmin <farjanayesmin76@gmail.com>
2026-05-06 03:55:03 +0000

    Fix ReadTheDocs build: add mock imports, docs/requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,24 @@
-import os, sys
-sys.path.insert(0, os.path.abspath(".."))
+import os
+import sys
 
-project   = "FairHealth"
-author    = "Farjana Yesmin"
-release   = "0.1.0"
-copyright = "2026, Farjana Yesmin"
+# Point to the package root
+sys.path.insert(0, os.path.abspath('..'))
+
+project   = 'FairHealth'
+author    = 'Farjana Yesmin'
+release   = '0.1.0'
+copyright = '2026, Farjana Yesmin'
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 napoleon_google_docstring = True
+autodoc_mock_imports = [
+    'torch', 'tenseal', 'shap', 'lime',
+    'skfuzzy', 'xgboost', 'wfdb', 'ucimlrepo',
+    'sklearn', 'scipy', 'matplotlib', 'pandas', 'numpy',
+]
